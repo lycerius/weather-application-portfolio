1a5882deb149468fc4b57fd64567fe5edae9e3b6
Nathan Purpura <purpuranc@gmail.com>
2026-05-06 20:58:16 -0400

    Add cluster

--- infra/Program.cs
+++ infra/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Pulumi;
+using Pulumi.Aws.Ecs;
 using Pulumi.Awsx.Ecs;
 using Pulumi.Awsx.Ecs.Inputs;
 using Pulumi.Awsx.Lb;
@@ -7,12 +8,14 @@ using Pulumi.Awsx.Lb;
 return await Deployment.RunAsync(() => 
 {
     var lb = new ApplicationLoadBalancer("nginx-lb");
+    var cluster = new Cluster("nginx-cluster");
 
 
     // Define the Fargate Service using the official nginx image
     var service = new FargateService("nginx-service", new FargateServiceArgs
     {
         AssignPublicIp = true,
+        Cluster = cluster.Arn,
         TaskDefinitionArgs = new Pulumi.Awsx.Ecs.Inputs.FargateServiceTaskDefinitionArgs
         {
             Container = new Pulumi.Awsx.Ecs.Inputs.TaskDefinitionContainerDefinitionArgs
