8269d8eb6ad905634e9a51a324641bfef33a20ae
Nathan Purpura <purpuranc@gmail.com>
2026-05-06 20:13:49 -0400

    Really getting there with nginx test

--- infra/Program.cs
+++ infra/Program.cs
@@ -1,15 +1,31 @@
-﻿using Pulumi;
-using Pulumi.Aws.S3;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Pulumi;
+using Pulumi.Awsx.Ecs;
+using Pulumi.Awsx.Ecs.Inputs;
+using Pulumi.Awsx.Lb;
 
-return await Deployment.RunAsync(() =>
+return await Deployment.RunAsync(() => 
 {
-    // Create an AWS resource (S3 Bucket)
-    var bucket = new Bucket("my-bucket");
-
-    // Export the name of the bucket
-    return new Dictionary<string, object?>
+    // Define the Fargate Service using the official nginx image
+    var service = new FargateService("nginx-service", new FargateServiceArgs
     {
-        ["bucketName"] = bucket.Id
-    };
+        TaskDefinitionArgs = new Pulumi.Awsx.Ecs.Inputs.FargateServiceTaskDefinitionArgs
+        {
+            Container = new Pulumi.Awsx.Ecs.Inputs.TaskDefinitionContainerDefinitionArgs
+            {
+                Name = "nginx",
+                Image = "nginx:latest", // Pulls directly from Docker Hub
+                Memory = 512,
+                Cpu = 256,
+                PortMappings = new List<TaskDefinitionPortMappingArgs>
+                {
+                    new TaskDefinitionPortMappingArgs
+                    {
+                        ContainerPort = 80,
+                        TargetGroup = new ApplicationLoadBalancer("nginx-lb").DefaultTargetGroup
+                    }
+                }
+            }
+        }
+    });
 });
